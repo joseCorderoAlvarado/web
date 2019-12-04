b526d98656e0247be2ee032cc12948781e3bab27
José Cordero Alvarado <48689264+joseCorderoAlvarado@users.noreply.github.com>
2019-12-03 22:29:04 -0600

    Update AltaUsuario.php

--- AltaUsuario.php
+++ AltaUsuario.php
@@ -10,11 +10,11 @@ ob_start("ob_gzhandler");
 <script src="js/jquery-1.11.0.min.js"></script>
 <!-- Custom Theme files -->
 <!--theme-style-->
-<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />	
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
 <!--//theme-style-->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="Free Style Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+<meta name="keywords" content="Free Style Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template,
 Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 <!--fonts-->
@@ -25,29 +25,29 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <script type="text/javascript" src="js/easing.js"></script>
 <script type="text/javascript">
 					jQuery(document).ready(function($) {
-						$(".scroll").click(function(event){		
+						$(".scroll").click(function(event){
 							event.preventDefault();
 							$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
 						});
 					});
-				</script>	
+				</script>
 <!-- start menu -->
 <script src="js/simpleCart.min.js"> </script>
 <link href="css/memenu.css" rel="stylesheet" type="text/css" media="all" />
 <script type="text/javascript" src="js/memenu.js"></script>
-<script>$(document).ready(function(){$(".memenu").memenu();});</script>				
+<script>$(document).ready(function(){$(".memenu").memenu();});</script>
 </head>
-<?php	
+<?php
 	error_reporting(E_ALL^E_NOTICE);
 		if(!$_GET)
 		{
-			header('Location:AltaUsuario.php?pagina=1'); //indicamos el inicio de la pagina mediante el método get 
+			header('Location:AltaUsuario.php?pagina=1'); //indicamos el inicio de la pagina mediante el método get
 		}
 	   $hombre=$_REQUEST['cate'];
 	   $mujer=$_REQUEST['cate'];
 	   $nino=$_REQUEST['cate'];
 	?>
-<body> 
+<body>
 	<!--top-header-->	<!--Busqueda, logo, Carrito-->
 	<div class="top-header">
 	<div class="container">
@@ -78,27 +78,27 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 										<li><a href="index1.php?cate=hombre&pagina=1">Hombres</a></li>
 										<li><a href="index1.php?cate=mujer&pagina=1">Mujeres</a></li>
 										<li><a href="index1.php?cate=nino&pagina=1">Ni&ntilde;os</a></li>
-										
+
 									</ul>
 								</div>
-								
+
 							</div>
 						</div>
-					</li> 
-			
+					</li>
+
 					<li class="grid"><a href="AltaUsuario.php">Registrarse</a>
-					</li> 
-					
+					</li>
+
 					<li class="grid"><a href="IniciarSesion.php">Iniciar Sesi&oacute;n</a>
-					</li> 
-					
+					</li>
+
 					<li class="grid"><a href="ayuda1.php">Ayuda</a>
 					</li>
 				</ul>
-			</div> 
+			</div>
 			<div class="clearfix"> </div>
 		</div>
-	</div> 
+	</div>
 		<!--banner-starts-->
 	<div class="bnr" id="home">
 		<div  id="top" class="callbacks_container">
@@ -116,7 +116,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		</div>
 		<div class="clearfix"> </div>
 	</div>
-	<!--banner-ends--> 
+	<!--banner-ends-->
 	<!--\\Slider-Starts-Here-->
 				<script src="js/responsiveslides.min.js"></script>
 			 <script>
@@ -136,12 +136,12 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 			          $('.events').append("<li>after event fired.</li>");
 			        }
 			      });
-			
+
 			    });
 			  </script>
 	<!--start-account-->
 	<div class="account">
-		<div class="container"> 
+		<div class="container">
 			<div class="account-bottom">
 				<div class="col-md-6 account-left">
 					<form method='POST'>
@@ -164,33 +164,33 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 						<input type="text" id="ApellidoM" name="ApellidoM" required=""  pattern="[A-Za-z]+"maxlength="20">
 					</div>
 					<div class="address">
-						Genero: <select name="Genero" id="Genero"  style="height: 40px; width: 50%;" required=""> 
+						Genero: <select name="Genero" id="Genero"  style="height: 40px; width: 50%;" required="">
 						<p></p>
            <?php
            error_reporting(E_ALL^E_NOTICE);
            include('Conexion.php');
            $ConsultaGenero="Select * FROM genero";
            $RCGenero=mysqli_query($conect,$ConsultaGenero)  or die("error al consultar los datos
-del usuario");   
+del usuario");
 while ($valores=mysqli_fetch_array($RCGenero)){
 echo "'<option value= $valores[idGenero]> $valores[Genero] </option>'";
                  }
             ?>
     			 </select>
 					</div>
-					
+
 					<div class="address">
 						Fecha de Nacimiento: <input type="date" id="FechaN" name="FechaN" required="required"> <p></p>
 					</div>
 					<div>
 						<h2>Datos de usuario</h2>
 					</div>
-					
+
 					<div class="address">
 						<span>Correo eletr&oacute;nico:</span>
 						<input type="text" name="Correo" id="Correo" required="required"   pattern="[A-Za-z@.]+">
 					</div>
-					
+
 					<div class="address">
 						<span>Contrase&ntilde;a:</span>
 						<input type="text" name="Contrasena" id="Contrasena" required=""  minlength="8" maxlength="20">
@@ -237,7 +237,7 @@ echo "'<option value= $valores[idGenero]> $valores[Genero] </option>'";
 						<input type="text" disabled placeholder="De envío" value="De envio" required="">
 					</div>
 				</div>
-			
+
 				<div class="address new">
 						<input type="submit" value="Registrarse" id="Registro"  name="Registro" style="margin: 25px;" required="" onclick="return confirm('Tu registro fue exitoso');">
 					</div>
@@ -250,25 +250,25 @@ echo "'<option value= $valores[idGenero]> $valores[Genero] </option>'";
 <?php
 //----------------------------Funcion para obtener la llave publica------------------------------------------------------------------
 
-function generarCodigos($cantidad=1, $longitud=1, $incluyeNum=true){ //funcion ramdom de llaves publicas para la contrasena donde la longitud del ramdom es 1 y la cantidad es 1 
+function generarCodigos($cantidad=1, $longitud=1, $incluyeNum=true){ //funcion ramdom de llaves publicas para la contrasena donde la longitud del ramdom es 1 y la cantidad es 1
     $caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //determinamos los caracteres
     if($incluyeNum) //en este caso no incluye numeros
-        $caracteres .= "1234567890"; 
+        $caracteres .= "1234567890";
     $arrPassResult=array(); //ese array generado lo guadamos en una variable
     $index=0; //generamos una variable de condicion
    while($index<$cantidad){ //mientras  index sea menor a cantidad metete a un cliclo
-        $tmp=""; //se crea una variable vacia
+        $tmp=""; //se crea una variable vaciaf
         for($i=0;$i<$longitud;$i++){ //mientras i<1 ramdoniza y guarda en la variable, ademas ve restando un caracter a la ramdonizacion
             $tmp.=$caracteres[rand(0,strlen($caracteres)-1)]; 
-        } 
-        if(!in_array($tmp, $arrPassResult)){//si no estamos en el array el resultado del ramdom velo guardando en el arreglo 
-            $arrPassResult[]=$tmp; 
-            $index++; 
-        } 
-    } 
+        }
+        if(!in_array($tmp, $arrPassResult)){//si no estamos en el array el resultado del ramdom velo guardando en el arreglo
+            $arrPassResult[]=$tmp;
+            $index++;
+        }
+    }
    return $arrPassResult; //retorna el resultado del arreglo
-}  
-$codigos=generarCodigos(1,1); //mandamos a llamar la funcion y le decimos que tendra un tamano de uno y un resultado de uno 
+}
+$codigos=generarCodigos(1,1); //mandamos a llamar la funcion y le decimos que tendra un tamano de uno y un resultado de uno
 $LlavePublica=implode($codigos); //convertimos ese arreglo a un string y lo guardamos en una variable
 $contra=$_POST["Contrasena"];
 $contra=$contra.=$LlavePublica; //a la contrasena le agregamos la llave publica generada por la funcion ramdom
@@ -298,7 +298,7 @@ $LlavePrivada=end($VectorPublico);
 //----------------------------------------------Encriptador--------------------------------------------------------------------------
 
 if ($clavePublica==$LlavePrivada) {
-for ($i=0; $i <$longitudvector-1 ; $i++) { 
+for ($i=0; $i <$longitudvector-1 ; $i++) {
 $resultadoBusqueda=array_search($vectorcontrasena[$i],$ArrayBase);
 $vectorcontrasena[$i]=$VectorPublico[$resultadoBusqueda];
 }
@@ -319,12 +319,12 @@ $NumeroE=$_POST["NumeroE"];     $Telefono=$_POST["Telefono"];
 $NumeroI=$_POST["NumeroI"];
 $Correo=$_POST["Correo"];     $Contrasena=$NuevaContrasena;
 $FechaN=$_POST["FechaN"];     $FechaNB= date("Y-m-d", strtotime($FechaN));
-$proc="call InsertUser($Genero,'$Nombre','$ApellidoP', '$ApellidoM','$FechaNB',NULL,'$Estado','$Municipio',NULL,'$Calle', 
+$proc="call InsertUser($Genero,'$Nombre','$ApellidoP', '$ApellidoM','$FechaNB',NULL,'$Estado','$Municipio',NULL,'$Calle',
 '$NumeroI',$CodigoPostal,'$NumeroE',1,NULL,NULL, '$Telefono','$Correo','$Contrasena',3)";
 mysqli_query($conect,$proc)  or die("error al insertar los datos
-del usuario");  
+del usuario");
 header("location:AltaUsuario.php");
-    
+
  }
 ?>
 
@@ -338,17 +338,17 @@ header("location:AltaUsuario.php");
 		</div>
 		<script type="text/javascript">
 									$(document).ready(function() {
-										
+
 										var defaults = {
 								  			containerID: 'toTop', // fading element id
 											containerHoverID: 'toTopHover', // fading element hover id
 											scrollSpeed: 1200,
-											easingType: 'linear' 
+											easingType: 'linear'
 								 		};
-										
-										
+
+
 										$().UItoTop({ easingType: 'easeOutQuart' });
-										
+
 									});
 								</script>
 
@@ -356,15 +356,15 @@ header("location:AltaUsuario.php");
 						$('#Cantidad').change(function(){
 							var CantidadProduc=$(this).val();
 							document.getElementById('resultadoCantidad').innerHTML=CantidadProduc;
-							
+
 						});
 
 					</script>
 		<a href="#home" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
 	</div>
-	<!--end-footer-text-->	
+	<!--end-footer-text-->
 </body>
 </html>
-<?php  
+<?php
 ob_end_flush();
 ?>
